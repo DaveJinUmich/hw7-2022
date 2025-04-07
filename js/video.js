11ca6f54aaa57e671df3030ce882088a64f749ba
DaveJinUmich <146284514+DaveJinUmich@users.noreply.github.com>
2025-04-06 20:22:26 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,10 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+// Initializing the video when the window loads
+window.addEventListener("load", function() {
+	console.log("Good j
